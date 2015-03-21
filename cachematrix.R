636d0e6f0eba2e827d3b35749b401a645cf507ec
Brendan McDougall <bamcdougall@yahoo.com>
2015-03-20 19:23:19 -0700

    Added comments and code to CacheMatrix based on makeVector and cacheMean

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+####
+## The actions of these two code blocks is storing a matrix (Mtrx), generating its inverse (MtrxInv),
+## and storing both the matrix and its inverse in a data.frame separate from the originating data.frame.
+##
+## The purpose of these actions is computational efficiency.  Generating inverse matrices is computationally
+## intensive.  If the originating matrix is not changing, then each call to its inverse need not be 
+## generated each time its needed.
+##
+## For more detail regarding lexical scope, see http://cran.r-project.org/doc/manuals/r-release/R-intro.html#Scope
+## (Section 10.7 Scope)
 
 ## Write a short comment describing this function
+####
+## makeCacheMatrix is a function that stores a matrix and its inverse.  Once the inverse matrix is
+## generated, subsequent calls return the buffered inverse matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(Mtrx = matrix()) {
+    MtrxInv <- NULL
+    set <- function(y) {
+        Mtrx <<- y
+        MtrxInv <<- NULL
+    }
+    get <- function() Mtrx
+    setmean <- function(mean) MtrxInv <<- mean
+    getmean <- function() MtrxInv
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
 }
 
 
 ## Write a short comment describing this function
+####
+## This function generated the inverse matrix object that is subsequently stored 
+## in the makeCacheMatrix method
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(Mtrx, ...) {
+        ## Return a matrix that is the inverse of 'Mtrx'
+    MtrxInv <- Mtrx$getmean()
+    if(!is.null(MtrxInv)) {
+        message("getting cached data")
+        return(MtrxInv)
+    }
+    data <- Mtrx$get()
+    MtrxInv <- mean(data, ...)
+    Mtrx$setmean(MtrxInv)
+    MtrxInv
 }
