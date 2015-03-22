c2765804d0d452dc513b028000493ea933a12a9c
Brendan McDougall <bamcdougall@yahoo.com>
2015-03-22 10:00:30 -0700

    3rd revision:  alignment from mk, cache vector to mk, cache inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,16 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 ####
-## The actions of these two code blocks is storing a matrix (Mtrx), generating its inverse (MtrxInv),
+## The actions of these two function (method) blocks is storing a matrix (Mtrx), generating its inverse (MtrxInv),
 ## and storing both the matrix and its inverse in a data.frame separate from the originating data.frame.
 ##
-## The purpose of these actions is computational efficiency.  Generating inverse matrices is computationally
-## intensive.  If the originating matrix is not changing, then each call to its inverse need not be 
-## generated each time its needed.
+## The purpose of these actions is computational efficiency.  Generating inverse matrices is ~O(n^3) computation.
+## (See http://en.wikipedia.org/wiki/Computational_complexity_of_mathematical_operations)
+## If the originating matrix is not changing, then each call to its inverse need not be 
+## calculated at each call.
 ##
-## For more detail regarding lexical scope, see http://cran.r-project.org/doc/manuals/r-release/R-intro.html#Scope
+## For more detail regarding lexical scope including the superassignment operator <<-,
+## msee http://cran.r-project.org/doc/manuals/r-release/R-intro.html#Scope
 ## (Section 10.7 Scope)
 
 ## Write a short comment describing this function
@@ -33,8 +35,9 @@ makeCacheMatrix <- function(Mtrx = matrix()) {
 
 ## Write a short comment describing this function
 ####
-## This function generated the inverse matrix object that is subsequently stored 
-## in the makeCacheMatrix method
+## This function generates the inverse matrix object that is subsequently stored 
+## in the makeCacheMatrix method.  cacheSolve is the primary function.  
+## makeCacheMatrix supports cacheSolve
 
 cacheSolve <- function(Mtrx, ...) {
         ## Return a matrix that is the inverse of 'Mtrx'
@@ -44,7 +47,14 @@ cacheSolve <- function(Mtrx, ...) {
         return(MtrxInv)
     }
     data <- Mtrx$get()
-    MtrxInv <- mean(data, ...)
+    MtrxInv <- solve(data, ...)
     Mtrx$setInverse(MtrxInv)
     MtrxInv
 }
+
+
+## One simple way to test functionally is generating a diagonal matrix using the Matrix library.
+## library(Matrix)
+## test <- 4*Diagonal(3,1)
+## BufferTest <- cacheSolve(test)BufferTest$getInverse()
+
